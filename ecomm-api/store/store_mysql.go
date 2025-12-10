f905ac05effc06f22abd695ef8cda63d48eaa3c5
Moses Omwa <mosesomwa7@gmail.com>
2025-12-10 16:28:13 +0300

    products

--- ecomm-api/store/store_mysql.go
+++ ecomm-api/store/store_mysql.go
@@ -1,1 +1,36 @@
 package store
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type MySQLStore struct {
+	db *sqlx.DB
+}
+
+func NewMySQLStore(db *sqlx.DB) *MySQLStore {
+	return &MySQLStore{db: db}
+}
+
+func (ms *MySQLStore) CreateProduct(ctx context.Context, p *Product) (*Product, error) {
+
+	res, err := ms.db.NamedExecContext(ctx,
+		`INSERT INTO products(name, image, category, description, rating, num_reviews, price, count_in_stock)
+         VALUES (:name, :image, :category, :description, :rating, :num_reviews, :price, :count_in_stock)`, p)
+
+	if err != nil {
+		return nil, fmt.Errorf("error inserting product: %v", err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, fmt.Errorf("error getting last insert id: %v", err)
+	}
+
+	p.ID = int64(int(id)) // assign the generated ID into the struct
+
+	return p, nil
+}
