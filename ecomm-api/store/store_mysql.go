80e688e453a08a759c3105ba84c8980353d124da
Moses Omwa <mosesomwa7@gmail.com>
2025-12-11 07:21:40 +0300

    server and handler

--- ecomm-api/store/store_mysql.go
+++ ecomm-api/store/store_mysql.go
@@ -44,3 +44,44 @@ func (ms *MySQLStore) GetProduct(ctx context.Context, id int64) (*Product, error
 
 	return &p, nil
 }
+func (ms *MySQLStore) UpdateProduct(ctx context.Context, p *Product) (*Product, error) {
+	query := `
+		UPDATE products
+		SET name = :name,
+		    image = :image,
+		    category = :category,
+		    description = :description,
+		    rating = :rating,
+		    num_reviews = :num_reviews,
+		    price = :price,
+		    count_in_stock = :count_in_stock
+		WHERE id = :id;
+	`
+
+	// using NamedExec because we are passing a struct with tags
+	_, err := ms.db.NamedExecContext(ctx, query, p)
+	if err != nil {
+		return nil, fmt.Errorf("error updating product: %v", err)
+	}
+
+	return p, nil
+}
+func (ms *MySQLStore) DeleteProduct(ctx context.Context, id int64) error {
+	query := `DELETE FROM products WHERE id = ?`
+
+	res, err := ms.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("error deleting product: %v", err)
+	}
+
+	affected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("error checking delete result: %v", err)
+	}
+
+	if affected == 0 {
+		return fmt.Errorf("no product found with id %d", id)
+	}
+
+	return nil
+}
