9312663f213ceebcefd5234c1d29784a8bd11703
Moses Omwa <mosesomwa7@gmail.com>
2025-12-15 12:19:38 +0300

    notification

--- ecomm-notification/server.go
+++ ecomm-notification/server.go
@@ -73,7 +73,7 @@ func (s *Server) sendNotification(ctx context.Context, ev *pb.NotificationEvent)
 	m.SetHeader("Subject", "email from ecomm")
 	m.SetBody("text/plain", fmt.Sprintf("Order %d is %s", strings.ToLower(ev.OrderStatus.String())))
 
-	gomail.NewDialer("stmp.gmail.com", 465, s.adminInfo.Email, s.adminInfo.Password).DialAndSend(m)
+	gomail.NewDialer("smtp.gmail.com", 465, s.adminInfo.Email, s.adminInfo.Password).DialAndSend(m)
 	return nil
 
 }
